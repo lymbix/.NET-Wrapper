1d1b7ba31cd023306bf6a66570424093d1c5603d
Josh Merchant <josh@lymbix.com>
2011-05-16 11:44:43 -0300

    First commit

---
+++ Lymbix.ToneAPI/Models/IntenseSentence.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Lymbix.ToneAPI.Models
+{
+    [Serializable]
+    [JsonObject]
+    public class IntenseSentence
+    {
+        [JsonProperty(PropertyName = "sentence")]
+        public string Sentence;
+        [JsonProperty(PropertyName = "dominant_emotion")]
+        public string Dominant_emotion;
+        [JsonProperty(PropertyName = "intensity")]
+        public double? Intensity;
+    }
+}
---
+++ Lymbix.ToneAPI/Models/Sentiment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Lymbix.ToneAPI.Models
+{
+    [Serializable]
+    [JsonObject]
+    public class Sentiment
+    {
+        [JsonProperty(PropertyName = "sentiment")]
+        public string SentimentType;
+        [JsonProperty(PropertyName = "score")]
+        public double? Score;
+    }
+}
