38d74ccb03be91f93091d550611c8e5a4f05213d
unknown <bsmith@LYMBIX-BSMITH.lymbix.com>
2012-02-06 13:51:27 -0400

    Added a version for .net 3.5 with an example

---
+++ ToneAPI/Example/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lymbix.ToneAPI;
+using Lymbix.ToneAPI.Models;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToneAPI toneapi = new ToneAPI("YOUR API KEY");
+            ArticleInfo response = toneapi.TonalizeDetailed("This is a beautiful world, don't you think?");
+            Console.WriteLine(String.Format("Contentment: {0}", response.ContentmentGratitude));
+        }
+    }
+}
